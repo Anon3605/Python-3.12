9733df7dc57b3031ac4a256fa245dd5c99844238
Arafat Sarkar <arafatgovt01@gmail.com>
2025-01-23 16:54:54 +0600

    commit 01

---
+++ IntroCodes/01_basics101.py
@@ -0,0 +1,5 @@
+print("Here are the Contents:")
+list=["Hierarchy", "Multi=Line Statements and Strings", "Naming Conventions", "Conditionals", "Functions", "Loops"]
+
+for i in range(len(list)):
+    print(f"{i} ==> {list[i]}")
